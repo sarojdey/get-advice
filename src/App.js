a11bfc36e72ca66fdfeac1b14b96b78499ecaf7d
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2023-11-05 14:08:52 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="content">
+        <h1 className="advice">Want some advice??</h1>
+        <button type="button" className="btn btn-primary">
+          Get advice
+        </button>
+      </div>
+    </>
   );
 }
 
