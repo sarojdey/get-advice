cb6bb491e1a6b1f6146ff71d8d25fee31fe700e4
Saroj Kumar Dey <sarojkumardey.contact@gmail.com>
2023-11-05 14:29:22 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [advice, setAdvice] = useState("");
+  const [advice, setAdvice] = useState("Want some advice?");
+  
 
+  async function getAdvice(){
+    const res = await  fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
 
   return (
     <>
       <div className="content">
-        <h1 className="advice">Want some advice??</h1>
-        <button type="button" className="btn btn-primary">
+        <h1 className="advice">{advice}</h1>
+        <button type="button" onClick={getAdvice} className="btn btn-primary">
           Get advice
         </button>
       </div>
